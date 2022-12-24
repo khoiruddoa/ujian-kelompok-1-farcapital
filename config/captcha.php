3208c6a26c6240f96f91912be4c3a44875c627e1
YOUR NAME <YOUR EMAIL@SAMLECOM>
2022-12-24 14:06:46 +0700

    nambah

--- config/captcha.php
+++ config/captcha.php
@@ -8,9 +8,9 @@ return [
         'width' => 120,
         'height' => 36,
         'quality' => 90,
-        'math' => true,
+        'math' => false,
         'expire' => 60,
-        'encrypt' => false,
+        'encrypt' => true,
     ],
     'math' => [
         'length' => 9,
